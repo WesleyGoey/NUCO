aa741ecace0606f73cdca5f1041860a0945985e5
WesleyGoey <wesley.goey@gmail.com>
2025-12-04 22:17:38 +0700

    database

--- NUCO/app/Models/User.php
+++ NUCO/app/Models/User.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -13,9 +12,10 @@ class User extends Authenticatable
     use HasFactory, Notifiable;
 
     protected $fillable = [
-        'name',
+        'username',
         'email',
         'password',
+        'phone',
         'role',
         'status'
     ];
--- NUCO/database/migrations/0001_01_01_000000_create_users_table.php
+++ NUCO/database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone');
             $table->enum('role', ['owner', 'waiter', 'chef', 'cashier', 'reviewer', 'guest'])->default('guest');
             $table->enum('status', ['active', 'inactive'])->default('active');
             $table->rememberToken();
