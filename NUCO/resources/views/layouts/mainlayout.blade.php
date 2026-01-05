d0230deb47369be434de9c0e74425a347d7bc5ae
WesleyGoey <wesley.goey@gmail.com>
2026-01-05 10:29:18 +0700

    Update mainlayout.blade.php

--- NUCO/resources/views/layouts/mainlayout.blade.php
+++ NUCO/resources/views/layouts/mainlayout.blade.php
@@ -8,9 +8,8 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <!-- Pastikan navigation.css dimuat setelah app.css agar override -->
     <link rel="stylesheet" href="{{ asset('css/navigation.css') }}">
-    <!-- Tambahkan di sini -->
-    <link rel="icon" href="/favicon.ico">
 </head>
 
 <body>
